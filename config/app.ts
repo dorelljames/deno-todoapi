dc14105994128e05be80b973fc38a62f758bab57
Dorell James <galangdj@gmail.com>
2020-06-07 12:03:41 +0800

    Resolve mongo dbname

--- config/app.ts
+++ config/app.ts
@@ -10,6 +10,7 @@ const env = config();
 export const PORT = env.PORT || 3000;
 export const BASE_URL = env.BASE_URL || "http://localhost";
 export const MONGO_URL = env.MONGO_URL || "mongodb://localhost:27017";
+export const MONGO_DBNAME = env.MONGO_DBNAME || "deno_todoapi";
 
 export const handleError = (
   err: any,
--- config/db.ts
+++ config/db.ts
@@ -1,9 +1,9 @@
 import { MongoClient } from "https://deno.land/x/mongo@v0.8.0/mod.ts";
-import { MONGO_URL } from "./app.ts";
+import { MONGO_URL, MONGO_DBNAME } from "./app.ts";
 
 const client = new MongoClient();
 client.connectWithUri(MONGO_URL);
 
-const db = client.database("deno_todoapi");
+const db = client.database(MONGO_DBNAME);
 
 export default db;
